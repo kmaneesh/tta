c1bf5d4ddd0b1c36e5a518e3eeac43269310879a
kmaneesh <maneesh.irs@gmail.com>
2020-06-12 23:29:39 +0800

    Updated the analysis

--- simulation/annual.py
+++ simulation/annual.py
@@ -26,13 +26,46 @@ class Annual(object):
         data_out = self.comtrade.get_data(self.reporter_area, self.partner_area, 2, period)  # export
         time.sleep(1)
         data_in = self.comtrade.get_data(self.partner_area, self.reporter_area, 1, period)  # import
-        return data_out, data_in
+        return self.compare(data_out, data_in)
 
     def compare_import(self, period=2019):
         data_in = self.comtrade.get_data(self.reporter_area, self.partner_area, 1, period)  # import
         time.sleep(1)
         data_out = self.comtrade.get_data(self.partner_area, self.reporter_area, 2, period)  # export
-        return data_in, data_out
+        return self.compare(data_in, data_out)
 
     def compare(self, data_a, data_b):
-        pass
+        data = {}
+        for item in data_a['dataset']:
+            if item['cmdCode'] in data:
+                print("Duplicate cmdCode found")
+            else:
+                data[item['cmdCode']] = {
+                    'desc': item['cmdDescE'],
+                    'quantity_a': item['NetWeight'],
+                    'quantity_desc_a': item['qtDesc'],
+                    'value_a': item['TradeValue'],
+                    'quantity_b': 0,
+                    'quantity_desc_b': item['qtDesc'],
+                    'value_b': 0
+                }
+
+        for item in data_b['dataset']:
+            if item['cmdCode'] in data:
+                data[item['cmdCode']].update({
+                    'quantity_b': item['NetWeight'],
+                    'quantity_desc_b': item['qtDesc'],
+                    'value_b': item['TradeValue']
+                })
+            else:
+                data[item['cmdCode']] = {
+                    'desc': item['cmdDescE'],
+                    'quantity_a': 0,
+                    'quantity_desc_a': item['qtDesc'],
+                    'value_a': 0,
+                    'quantity_b': item['NetWeight'],
+                    'quantity_desc_b': item['qtDesc'],
+                    'value_b': item['TradeValue']
+                }
+        return data
+
