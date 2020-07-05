f9070315f397567a38ee873b10b0cea15d95a1f5
kmaneesh <maneesh.irs@gmail.com>
2020-07-05 08:15:16 +0800

    Updated the code

--- comtrade/api.py
+++ comtrade/api.py
@@ -61,14 +61,12 @@ class Api(object):
             }
             print(params) # debug
             r = requests.get(url=url, params=params)
-            try:
-                records = r.json()
-                if len(records['dataset']):
-                    data[year_month] = 0
-                    for item in records['dataset']:
-                        data[year_month] = data[year_month] + item['TradeValue']
-                else:
-                    data[year_month] = 0
-            except Exception as e:
+            records = r.json()
+            if len(records['dataset']):
+                data[year_month] = 0
+                for item in records['dataset']:
+                    data[year_month] = data[year_month] + item['TradeValue']
+
+            else:
                 data[year_month] = 0
         return data
